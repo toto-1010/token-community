3c83a48a2cec538b70204ef77654fa03b9f8d591
toto-1010 <to10san.apps@gmail.com>
2022-07-26 07:05:48 +0900

    MemberNFT:OZインポートと継承、constructor定義

--- contracts/MemberNFT.sol
+++ contracts/MemberNFT.sol
@@ -2,4 +2,48 @@
 
 pragma solidity ^0.8.9;
 
-contract MemberNFT {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MemberNFT is ERC721Enumerable, ERC721URIStorage, Ownable{
+    constructor() ERC721("MemberNFT","MEM") {}
+
+    /**
+     * @dev
+     * - オーバーライド
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    /**
+     * @dev
+     * - オーバーライド
+     */
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    /**
+     * @dev
+     * - オーバーライド
+     */
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721,ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @dev
+     * - オーバーライド
+     */
+    function tokenURI(uint256 tokenId) public view override(ERC721,ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+}
