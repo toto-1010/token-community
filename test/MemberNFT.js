bc89118b7cc9c4c835e510882bd8dac55bf73b25
toto-1010 <to10san.apps@gmail.com>
2022-07-27 07:37:35 +0900

    MemberNFT:テストの前処理実装

--- test/MemberNFT.js
+++ test/MemberNFT.js
@@ -2,20 +2,24 @@ const {expect} = require("Chai");
 const { ethers } = require("hardhat");
 
 describe("MemberNFTコントラクト", function() {
-    it("トークンの名前とシンボルがセットされるべき",async function() {
-        const name = "MemberNFT";
-        const symbol = "MEM";
-        const MemberNFT = await ethers.getContractFactory("MemberNFT");
-        const memberNFT = await MemberNFT.deploy();
+    let MemberNFT;
+    let memberNFT;
+    const name = "MemberNFT";
+    const symbol = "MEM";
+    let owner;
+
+    beforeEach(async function() {
+        [owner] = await ethers.getSigners();
+        MemberNFT = await ethers.getContractFactory("MemberNFT");
+        memberNFT = await MemberNFT.deploy();
         await memberNFT.deployed();
+    });
+    
+    it("トークンの名前とシンボルがセットされるべき",async function() {
         expect(await memberNFT.name()).to.equal(name);
         expect(await memberNFT.symbol()).to.equal(symbol);
     });    
     it("デプロイアドレスがownerに設定されるべき",async function() {
-        const [owner] = await ethers.getSigners();
-        const MemberNFT = await ethers.getContractFactory("MemberNFT");
-        const memberNFT = await MemberNFT.deploy();
-        await memberNFT.deployed();
         expect(await memberNFT.owner()).to.equal(owner.address);
     });    
 })
