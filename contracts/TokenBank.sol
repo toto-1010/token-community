e77a139f57d3239f632459bd583bc7680efa3fc4
toto-1010 <to10san.apps@gmail.com>
2022-07-31 07:39:09 +0900

    TokenBank:constructor定義

--- contracts/TokenBank.sol
+++ contracts/TokenBank.sol
@@ -42,4 +42,11 @@ contract TokenBank {
         address indexed from,
         uint256 amount
     );
+
+    constructor (string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+        owner = msg.sender;
+        _balances[owner] = _totalSupply;
+    }
 }
