2b11706158285fa8ec80a58bd99b5eac975da562
toto-1010 <to10san.apps@gmail.com>
2022-07-31 07:10:19 +0900

    TokenBank:イベント定義

--- contracts/TokenBank.sol
+++ contracts/TokenBank.sol
@@ -3,24 +3,43 @@
 pragma solidity ^0.8.9;
 
 contract TokenBank {
-    // Tokenの名前
+    /// @dev Tokenの名前
     string private _name;
 
-    // Tokenのシンボル
+    /// @dev Tokenのシンボル
     string private _symbol;
 
-    // Tokenの総供給数
+    /// @dev Tokenの総供給数
     uint256 constant _totalSupply = 1000;
 
-    // TokenBankが預かっているTokenの総額
+    /// @dev TokenBankが預かっているTokenの総額
     uint256 private _bankTotalDeposit;
 
-    // TokenBankのオーナー
+    /// @dev TokenBankのオーナー
     address public owner;
 
-    // アカウントアドレス毎のToken残高
+    /// @dev アカウントアドレス毎のToken残高
     mapping(address => uint256) private _balances;
 
-    // TokenBankが預かっているToken残高
+    /// @dev TokenBankが預かっているToken残高
     mapping(address => uint256) private _tokenBankBalances;    
+
+    /// @dev Token移転時のイベント
+    event TokenTransfer(
+        address indexed from,
+        address indexed to,
+        uint256 amount
+    );
+
+    /// @dev Token預入時のイベント
+    event TokenDeposit(
+        address indexed from,
+        uint256 amount
+    );
+
+    /// @dev Token引出時のイベント
+    event TokenWithdraw(
+        address indexed from,
+        uint256 amount
+    );
 }
