305a526fd441831b8c18e56574333442584503c0
toto-1010 <to10san.apps@gmail.com>
2022-08-08 22:32:46 +0900

    frontend:トークン移転機能実装

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -99,6 +99,39 @@ export default function Home() {
     } else { '' }
   }
 
+  const tokenTransfer = async (event) => {
+    event.preventDefault();
+    if (tokenBalance >= inputData.transferAmount && zeroAddress != inputData.transferAddress) {
+      try{
+        const { ethereum } = window;
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const tokenBankContract = new ethers.Contract(tokenBankAddress, TokenBank.abi, signer);
+        const tx = await tokenBankContract.transfer(inputData.transferAddress, inputData.transferAmount);
+        await tx.wait();
+
+        const tBalance = await tokenBankContract.balanceOf(account);
+        setTokenBalance(tBalance.toNumber());
+        setInputData(prevData => ({
+          ...prevData,
+          transferAddress: '',
+          transferAmount: ''
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      alert("所持残高を超えるトークン及びゼロアドレス宛は指定できません")
+    }
+  }
+
+  const handler = (e) => {
+    setInputData(prevData => ({
+      ...prevData,
+      [e.target.name]: e.target.value
+    }));
+  }
+
   useEffect(() => {
     checkMetaMaskInstalled()
     checkChainId()
@@ -144,6 +177,31 @@ export default function Home() {
                 <span className="flex flex-col items-left font-semibold">所持残高：{tokenBalance}</span>
                 < span className="flex flex-col items-left font-semibold">預入残高：{bankBalance}</span>
               </div>
+              {nftOwner ? (
+              <>
+                <form className="flex pl-1 py-1 mb-1 bg-white border border-gray-400">
+                  <input
+                    type="text"
+                    className="w-5/12 ml-2 text-center border border-gray-400"
+                    name="transferAddress"
+                    placeholder="Wallet Address"
+                    onChange={handler}
+                    value={inputData.transferAddress}
+                  />
+                  <input
+                    type="text"
+                    className="w-5/12 ml-2 text-right border border-gray-400"
+                    name="transferAmount"
+                    placeholder={`100`}
+                    onChange={handler}
+                    value={inputData.transferAmount}
+                  />
+                  <button
+                    className="w-2/12 mx-2 bg-white border-blue-500 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded"
+                    onClick={tokenTransfer}
+                  >移転</button>
+                </form>
+              </>) : (<></>)}
             </div>
           ) : (
             <div className='flex flex-col justify-center items-center mb-20 font-bold text-2xl gap-y-3'>
