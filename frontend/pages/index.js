d4e975ca63c1e2c8f989a13d58e3ae545648990d
toto-1010 <to10san.apps@gmail.com>
2022-08-08 17:25:44 +0900

    frontend:MemberNFTクエリとNFT所有チェック機能実装

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -67,6 +67,8 @@ export default function Home() {
       console.log(`totalDeposit: ${totalDeposit}`);
       setBankTotalDeposit(totalDeposit.toNumber());
 
+      checkNft(accounts[0]);
+
       ethereum.on('accountsChanged', checkAccountChanged);
       ethereum.on('chainChanged', checkChainId);
     } catch (err) {
@@ -82,6 +84,19 @@ export default function Home() {
     setBankBalance('');
     setBankTotalDeposit('');
     setInputData({ transferAddress: '', transferAmount: '', depositAmount: '', withdrawAmount: '' });
+  }
+
+  const checkNft = async (addr) => {
+    const { ethereum } = window;
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const memberNFTContract = new ethers.Contract(memberNFTAddress, MemberNFT.abi, signer);
+    const balance = await memberNFTContract.balanceOf(addr);
+    console.log(`nftBalance: ${balance.toNumber()}`);
+
+    if (balance.toNumber() > 0) {
+      setNftOwner(true);
+    } else { '' }
   }
 
   useEffect(() => {
