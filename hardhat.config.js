13ca0bb64897fe4f6e745520280466e0099342b9
とと <104406108+toto-1010@users.noreply.github.com>
2022-09-25 13:15:27 +0900

    Update hardhat.config.js
    Goerli対応

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,8 +8,8 @@ require("@nomiclabs/hardhat-etherscan");
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    rinkeby: {
-      url:`https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+    goerli: {
+      url:`https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY],
     }
   },
