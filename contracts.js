8b8d4b08f3e91dbcbb0f204f9d2a6db419499903
とと <104406108+toto-1010@users.noreply.github.com>
2022-10-28 07:05:11 +0900

    Update contracts.js
    Goerliへの変更対応

--- contracts.js
+++ contracts.js
@@ -1,4 +1,4 @@
 
-    export const memberNFTAddress = "0x1661a8645EA26C680C07c086a0B74e480ad2fcF9"
+    export const memberNFTAddress = "0xEDF11014DBD455698d5D744C56A741135F08C4C1"
     export const tokenBankAddress = "0x6f85A6c7C7Ce8611B0dEC568d9E8552f6F973787"
     
