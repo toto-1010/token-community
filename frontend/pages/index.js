0423bb9878cad9ee9de620d7e2b8eab9696c4162
toto-1010 <to10san.apps@gmail.com>
2023-04-07 10:52:04 +0900

    Goerli ETHが取得しづらくなったためMumbaiを使う前提のコードに変更4

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
   });
   const [items, setItems] = useState([]);
   // const goerliId = '0x5'
-  const mumbaiId = "0x80001";
+  const mumbaiId = "0x13881";
   const zeroAddress = "0x0000000000000000000000000000000000000000";
 
   const checkMetaMaskInstalled = async () => {
